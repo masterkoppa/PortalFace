9b55f4964dde782c5531234fe895fe70b35d5bb7
Andres Ruiz <andresruiz2010@gmail.com>
2013-05-06 19:40:38 -0400

    Add some pusher things

---
+++ MvcApplication1/MvcApplication1/Controllers/PusherController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+
+namespace MvcApplication1.Controllers
+{
+    public class PusherController : Controller
+    {
+        //
+        // GET: /Pusher/Auth
+        public ActionResult Auth(string channel_name, string socket_id)
+        {
+            var pusher = new Pusher(APP_ID, APP_KEY, APP_SECRET);
+            var auth = pusher.Authenticate(channel_name, socketId);
+            var json = auth.ToJson();
+            return new ContentResult { Content = json, ContentType = "application/json" };
+        }
+
+    }
+}
