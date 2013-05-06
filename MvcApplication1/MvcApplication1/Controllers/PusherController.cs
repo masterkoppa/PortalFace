60cb9cdfcd47f5fda7fcafa74560ef190db6da5c
Alex Kelley <ajk6045@rit.edu>
2013-05-06 19:54:00 -0400

    added auth info to controller

--- MvcApplication1/MvcApplication1/Controllers/PusherController.cs
+++ MvcApplication1/MvcApplication1/Controllers/PusherController.cs
@@ -13,7 +13,7 @@ namespace MvcApplication1.Controllers
         // GET: /Pusher/Auth
         public ActionResult Auth(string channel_name, string socket_id)
         {
-            var pusher = new Pusher(APP_ID, APP_KEY, APP_SECRET);
+            var pusher = new Pusher('43416', 'ab3a6a2e93203b02ee34', '634586f75f28130dbdef');
             var auth = pusher.Authenticate(channel_name, socketId);
             var json = auth.ToJson();
             return new ContentResult { Content = json, ContentType = "application/json" };
