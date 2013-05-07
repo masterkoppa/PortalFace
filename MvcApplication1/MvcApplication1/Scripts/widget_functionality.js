2aaf9da0c99ade537d1903a3aedc8de79b22d766
Andres Ruiz <andresruiz2010@gmail.com>
2013-05-07 04:34:16 -0400

    Misc Fixes
    Some fixes here and there with css

--- MvcApplication1/MvcApplication1/Scripts/widget_functionality.js
+++ MvcApplication1/MvcApplication1/Scripts/widget_functionality.js
@@ -77,7 +77,7 @@ var widget_functionality = {
                     return false;
                 }).appendTo($(settings.handleSelector,this));
                 $('<div class="edit-box" style="display:none;"/>')
-                    .append('<ul><li class="item"><label>Change the title?</label><input value="' + $('h3',this).text() + '"/></li>')
+                    //.append('<ul><li class="item"><label>Change the title?</label><input value="' + $('h3',this).text() + '"/></li>')
                     .append((function(){
                         var colorList = '<li class="item"><label>Available colors:</label><ul class="colors">';
                         $(thisWidgetSettings.colorClasses).each(function () {
