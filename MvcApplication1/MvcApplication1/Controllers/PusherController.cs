eca967791272a6d4651a3ebd7658b94d06ddaf04
Andres Ruiz <andresruiz2010@gmail.com>
2013-05-06 20:05:21 -0400

    Add the pusher, initial work done
    WOHO!!!

--- MvcApplication1/MvcApplication1/Controllers/PusherController.cs
+++ MvcApplication1/MvcApplication1/Controllers/PusherController.cs
@@ -13,8 +13,8 @@ namespace MvcApplication1.Controllers
         // GET: /Pusher/Auth
         public ActionResult Auth(string channel_name, string socket_id)
         {
-            var pusher = new Pusher('43416', 'ab3a6a2e93203b02ee34', '634586f75f28130dbdef');
-            var auth = pusher.Authenticate(channel_name, socketId);
+            var pusher = new Pusher("43416", "ab3a6a2e93203b02ee34", "634586f75f28130dbdef");
+            var auth = pusher.Authenticate(channel_name, socket_id);
             var json = auth.ToJson();
             return new ContentResult { Content = json, ContentType = "application/json" };
         }
